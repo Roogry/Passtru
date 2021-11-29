3252e2569dd0e62a0c1379aa4544374d472107e9
Prabhaisvari Sadhaka <76557114+prbisv@users.noreply.github.com>
2021-11-29 21:43:36 +0800

    add list sosmed & home screen

---
+++ app/src/main/java/id/roogry/passtru/ui/HomeScreenActivity.java
@@ -0,0 +1,14 @@
+package id.roogry.passtru.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import id.roogry.passtru.R;
+
+public class HomeScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+    }
+}
