5b6802d4ec6099e3dfb8bd17c0ac5adfc5986de1
menkz <komangwahyu@student.unud.ac.id>
2021-12-09 09:39:35 +0800

    add insert data sosmed

--- app/src/main/java/id/roogry/passtru/helpers/CustomDialog.java
+++ app/src/main/java/id/roogry/passtru/helpers/CustomDialog.java
@@ -2,17 +2,22 @@ package id.roogry.passtru.helpers;
 
 import android.app.Activity;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.os.AsyncTask;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import id.roogry.passtru.R;
+import id.roogry.passtru.database.PasstruRoomDatabase;
+import id.roogry.passtru.models.Sosmed;
 import id.roogry.passtru.ui.ListAccountActivity;
 
 public class CustomDialog {
@@ -26,6 +31,17 @@ public class CustomDialog {
     public void startAlertDialog(String type, String message, Integer view){
         dialog = new Dialog(activity);
         dialog.setContentView(view);
+
+        if (type.equals("form")){
+            Button btnSave = dialog.findViewById(R.id.btnSubmit);
+            EditText inputSosmed = dialog.findViewById(R.id.edtSosmed);
+            Sosmed sosmed = new Sosmed();
+            btnSave.setOnClickListener(v -> {
+                sosmed.setTitle(inputSosmed.getText().toString());
+                insertData(sosmed);
+            });
+        }
+
 
         dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
@@ -42,4 +58,19 @@ public class CustomDialog {
     }
 
 
+    private void insertData(final Sosmed sosmed){
+        PasstruRoomDatabase passtruRoomDatabase = PasstruRoomDatabase.getDatabase(activity);
+        new AsyncTask<Void, Void, Long>(){
+            @Override
+            protected Long doInBackground(Void... voids) {
+                // melakukan proses insert data
+                long status = passtruRoomDatabase.sosmedDao().insert(sosmed);
+                return status;
+            }
+            @Override
+            protected void onPostExecute(Long status) {
+                Toast.makeText(activity, "status row "+status, Toast.LENGTH_SHORT).show();
+            }
+        }.execute();
+    }
 }
--- app/src/main/java/id/roogry/passtru/ui/ListSosmedActivity.java
+++ app/src/main/java/id/roogry/passtru/ui/ListSosmedActivity.java
@@ -1,23 +1,35 @@
 package id.roogry.passtru.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.List;
 
 import id.roogry.passtru.R;
 import id.roogry.passtru.adapter.SosmedListAdapter;
 import id.roogry.passtru.databinding.ActivityListAccountBinding;
 import id.roogry.passtru.databinding.ActivityListSosmedBinding;
 import id.roogry.passtru.helpers.CustomDialog;
+import id.roogry.passtru.helpers.ViewModelFactory;
+import id.roogry.passtru.models.Account;
+import id.roogry.passtru.models.Sosmed;
+import id.roogry.passtru.viewmodel.HomeViewModel;
+import id.roogry.passtru.viewmodel.ListSosmedViewModel;
 
 public class ListSosmedActivity extends AppCompatActivity {
 
     private ActivityListSosmedBinding binding;
     private SosmedListAdapter sosmedListAdapter;
     private CustomDialog customDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,8 +37,15 @@ public class ListSosmedActivity extends AppCompatActivity {
         binding = ActivityListSosmedBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         customDialog = new CustomDialog(ListSosmedActivity.this);
+
+        ViewModelFactory factory = ViewModelFactory.getInstance(this.getApplication());
+        ListSosmedViewModel sosmedViewModel = new ViewModelProvider(this, factory).get(ListSosmedViewModel.class);
+//        sosmedViewModel.getSosmeds().observe(this, sosmedObersver);
+
+
         sosmedListAdapter = new SosmedListAdapter();
         binding.rvSosmedList.setLayoutManager(new LinearLayoutManager(this));
+        binding.rvSosmedList.setHasFixedSize(true);
         binding.rvSosmedList.setAdapter(sosmedListAdapter);
 
         binding.ivBack.setOnClickListener(v -> {
@@ -44,4 +63,11 @@ public class ListSosmedActivity extends AppCompatActivity {
         super.onDestroy();
         binding = null;
     }
+
+//    private final Observer<List<Sosmed>> sosmedObersver = sosmedList -> {
+//        if (sosmedList != null) {
+//            Log.d("testData", "ada datanya kok");
+////            sosmedListAdapter.setListAccounts(accountList);
+//        }
+//    };
 }
